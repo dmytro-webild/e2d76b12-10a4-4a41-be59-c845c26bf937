66b182e81ef9a84d8456af0ef025caac8cfa3514
bender <kudin.dima@gmail.com>
2026-06-09 15:46:16 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -131,7 +131,7 @@ export default function LandingPage() {
       metrics={[
         {
           id: "1",          icon: Users,
-          title: "Audience Reach Growth",          value: "6.5 mil"},
+          title: "Audience Reach Growth",          value: "6000000"},
         {
           id: "2",          icon: MessageCircle,
           title: "Add Partners Satisfaction ",          value: "95%"},
